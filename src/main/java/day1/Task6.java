febe0505bf2865fec64fa58e9e2d9e71618b349c
Alexandr_Shaydullin <cashAnDhand@yandex.ru>
2021-02-25 14:59:28 +0300

    Mission Complete

--- src/main/java/day1/Task6.java
+++ src/main/java/day1/Task6.java
@@ -1,7 +1,13 @@
 package day1;
 
 public class Task6 {
-    public static void main(String[] args) {
+	public static void main(String[] args) {
+		int k = 5;
 
-    }
+		for (int a = 1; a <= 9; a++) {
+
+			int b = a * k;
+			System.out.println(a + " x " + k + " = " + b);
+		}
+	}
 }
