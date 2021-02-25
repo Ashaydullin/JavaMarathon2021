45e7e32ac657a18ada86bf1a4bd27d63ebde9135
Alexandr_Shaydullin <cashAnDhand@yandex.ru>
2021-02-25 15:03:17 +0300

    mission complete

--- src/main/java/day2/Task1.java
+++ src/main/java/day2/Task1.java
@@ -1,7 +1,18 @@
 package day2;
 
-public class Task1 {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
-    }
+public class Task1 {
+	public static void main(String[] args) {
+		System.out.println(" Введите номер этажа ");
+		Scanner scn = new Scanner(System.in);
+		int a = scn.nextInt();
+		if (a >= 1 && a <= 4) {
+			System.out.println(" Малоэтажный дом! ");
+		} else if (a >= 5 && a <= 8) { 
+				System.out.println(" Среднееэтажный дом! ");
+			} else if (a >= 8 && a <= 90) { 
+			System.out.println(" Многоэтажный дом! ");
+		}
+	}
 }
